9b9e6526e29d7eaaa01ed70b19ec2e0b41ab8033
hhhhhq <Mr.j@tom.com>
2020-10-27 16:26:29 +0800

    添加简单过渡动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div v-if="visible" class="sider">
+      <slot></slot>
+      <button @click="visible = !visible">close</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-  name: "GSider"
+  name: "GSider",
+  data() {
+    return {
+      visible: true
+    }
+  },
+  methods: {}
 }
 </script>
 
-<style scoped>
-.sider {}
+<style lang="scss" scoped>
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active, .slide-leave-active {
+  transition: all .5s;
+}
+.slide-enter, .slide-leave-to {
+  margin-left: -200px;
+}
 </style>
