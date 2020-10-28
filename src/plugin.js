ef0a55d0edebde77c3b0f669058d1df3504b33cc
hhhhhq <Mr.j@tom.com>
2020-10-28 16:28:52 +0800

    添加 toast 和 plugin

---
+++ src/plugin.js
@@ -0,0 +1,13 @@
+import Toast from './toast'
+
+export default {
+  install(Vue, options) {
+    Vue.prototype.$toast = function(message) {
+      let Constructor = Vue.extend(Toast)
+      let vm = new Constructor()
+      vm.$slots.default = [message]
+      vm.$mount()
+      document.body.appendChild(vm.$el)
+    }
+  }
+}
---
+++ src/toast.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GToast"
+}
+</script>
+
+<style lang="scss" scoped>
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, .75);
+$border-radius: 4px;
+
+.toast {
+  position: fixed;top: 0;left: 50%;padding: 0 16px;height: $toast-height;font-size: $font-size;line-height: 1.8;transform: translateX(-50%);display: flex;align-items: center;background-color: $toast-bg;border-radius: $border-radius;color: #fff;box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+}
+</style>
