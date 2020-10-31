3c8eec6234e518d302d834faa2369aff77f51233
hhhhhq <Mr.j@tom.com>
2020-10-31 10:57:42 +0800

    添加 tabs 相关组件

--- src/app.js
+++ src/app.js
@@ -1,65 +1,47 @@
 import Vue from 'vue'
-import Button from './button'
+// import Button from './button'
 import Icon from './icon'
-import ButtonGroup from './button-group'
-import Input from './input'
-import Row from './row'
-import Col from './col'
-import Layout from './layout'
-import Header from './header'
-import Sider from './sider'
-import Content from './content'
-import Footer from './footer'
-import Toast from './toast'
-import plugin from "./plugin";
+// import ButtonGroup from './button-group'
+// import Input from './input'
+// import Row from './row'
+// import Col from './col'
+// import Layout from './layout'
+// import Header from './header'
+// import Sider from './sider'
+// import Content from './content'
+// import Footer from './footer'
+// import Toast from './toast'
+// import plugin from "./plugin";
+import Tabs from './tabs';
+import TabsHead from './tabs-head';
+import TabsBody from './tabs-body';
+import TabsItem from './tabs-item';
+import TabsPane from './tabs-pane';
 
-Vue.component('g-button', Button)
+// Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-input', Input)
-Vue.component('g-row', Row)
-Vue.component('g-col', Col)
-Vue.component('g-layout', Layout)
-Vue.component('g-sider', Sider)
-Vue.component('g-header', Header)
-Vue.component('g-content', Content)
-Vue.component('g-footer', Footer)
-Vue.component('g-toast', Toast)
-Vue.use(plugin)
+// Vue.component('g-button-group', ButtonGroup)
+// Vue.component('g-input', Input)
+// Vue.component('g-row', Row)
+// Vue.component('g-col', Col)
+// Vue.component('g-layout', Layout)
+// Vue.component('g-sider', Sider)
+// Vue.component('g-header', Header)
+// Vue.component('g-content', Content)
+// Vue.component('g-footer', Footer)
+// Vue.component('g-toast', Toast)
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-pane', TabsPane)
+// Vue.use(plugin)
 
 new Vue({
   el: '#app',
   data() {
     return {
-      loading1: false,
-      loading2: false,
-      loading3: false,
-      message: 'hi'
-    }
-  },
-  methods: {
-    showToast1() {
-      this.showToast('top')
-    },
-    showToast2() {
-      this.showToast('middle')
-    },
-    showToast3() {
-      this.showToast('bottom')
-    },
-    showToast(position) {
-      this.$toast('单行文字，完', {
-        closeButton: {
-          text: '知道了',
-          callback(toast) {
-            toast.log()
-            console.log('用户说它知道了')
-          }
-        },
-        enableHtml: false,
-        autoClose: 3,
-        position
-      })
+      selectedTab: 'vue'
     }
   }
 })
