b7efb43110669f2ec3c04ac0e434462013a9e3ff
hhhhhq <Mr.j@tom.com>
2020-11-01 23:25:18 +0800

    创建 popover 组件

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 // import Button from './button'
-import Icon from './icon'
+// import Icon from './icon'
 // import ButtonGroup from './button-group'
 // import Input from './input'
 // import Row from './row'
@@ -12,14 +12,15 @@ import Icon from './icon'
 // import Footer from './footer'
 // import Toast from './toast'
 // import plugin from "./plugin";
-import Tabs from './tabs';
-import TabsHead from './tabs-head';
-import TabsBody from './tabs-body';
-import TabsItem from './tabs-item';
-import TabsPane from './tabs-pane';
+// import Tabs from './tabs';
+// import TabsHead from './tabs-head';
+// import TabsBody from './tabs-body';
+// import TabsItem from './tabs-item';
+// import TabsPane from './tabs-pane';
+import Popover from './popover'
 
 // Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
+// Vue.component('g-icon', Icon)
 // Vue.component('g-button-group', ButtonGroup)
 // Vue.component('g-input', Input)
 // Vue.component('g-row', Row)
@@ -30,24 +31,14 @@ Vue.component('g-icon', Icon)
 // Vue.component('g-content', Content)
 // Vue.component('g-footer', Footer)
 // Vue.component('g-toast', Toast)
-Vue.component('g-tabs', Tabs)
-Vue.component('g-tabs-head', TabsHead)
-Vue.component('g-tabs-body', TabsBody)
-Vue.component('g-tabs-item', TabsItem)
-Vue.component('g-tabs-pane', TabsPane)
+// Vue.component('g-tabs', Tabs)
+// Vue.component('g-tabs-head', TabsHead)
+// Vue.component('g-tabs-body', TabsBody)
+// Vue.component('g-tabs-item', TabsItem)
+// Vue.component('g-tabs-pane', TabsPane)
+Vue.component('g-popover', Popover)
 // Vue.use(plugin)
 
 new Vue({
   el: '#app',
-  data() {
-    return {
-      selectedTab: 'vue'
-    }
-  },
-  methods: {
-    yyy(data) {
-      console.log('yyy')
-      console.log(data)
-    }
-  }
 })
