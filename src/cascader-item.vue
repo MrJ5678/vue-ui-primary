85031a81e57c4d318c57e24df8328d6f9554af8f
hhhhhq <Mr.j@tom.com>
2020-11-05 11:15:04 +0800

    创建 cascader 组件

---
+++ src/cascader-item.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cascader-item">
+    {{ sourceItem.name }}
+    <g-cascader-item
+      v-if="sourceItem.children"
+      v-for="item in sourceItem.children"
+      :source-item="item"
+    >
+    </g-cascader-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GCascaderItem",
+  props: {
+    sourceItem: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cascader-item {
+  border: 1px solid red;
+  margin: 10px;
+}
+</style>
