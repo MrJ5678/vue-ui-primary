da5f5d10ff257bf0a409ac871a4d470acc8ca596
hhhhhq <Mr.j@tom.com>
2020-11-06 10:25:34 +0800

    确定如何渲染内容

---
+++ src/cascader-items.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="cascader-item">
+    <div class="left">
+      <div class="label" v-for="item in items" @click="leftSelected = item">
+        {{ item.name }}
+      </div>
+    </div>
+    <div class="right" v-if="rightItems">
+      <g-cascader-items :items="rightItems"></g-cascader-items>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GCascaderItems",
+  props: {
+    items: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      leftSelected: null
+    }
+  },
+  computed: {
+    rightItems() {
+      if(this.leftSelected && this.leftSelected.children) {
+        return this.leftSelected.children
+      } else {
+        return null
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cascader-item {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  .left { border: 1px solid red; }
+  .right { margin-top: -1px; }
+}
+</style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,34 +1,51 @@
 <template>
   <div class="cascader">
-    <div class="trigger">
-      <slot></slot>
+    <div class="trigger" @click="popoverVisible = !popoverVisible">
     </div>
-    <div class="popover">
-      <div v-for="item in source">
-        <cascader-item :sourceItem="item"></cascader-item>
-      </div>
+    <div class="popover" v-if="popoverVisible">
+      <cascader-items :items="source"></cascader-items>
     </div>
   </div>
 </template>
 
 <script>
-import CascaderItem from './cascader-item'
+import CascaderItems from './cascader-items'
 
 export default {
   name: "GCascader",
-  components: { CascaderItem },
+  components: { CascaderItems },
   props: {
     source: {
       type: Array
     }
-  }
-}
+  },
+  data() {
+    return {
+      popoverVisible: false,
+    }
+  }, }
 </script>
 
 <style lang="scss" scoped>
 @import "var";
 
 .cascader {
+  .trigger {
+    height: 32px;
+    width: 100px;
+    border: 1px solid red;
+  }
+
+  .popover {
+    display: flex;
+    height: 200px;
+    border: 2px solid green;
+
+    .label {
+      white-space: nowrap;
+    }
+  }
+
 
 }
 </style>
