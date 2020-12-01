3507305b70cc2a7fb3cdf65cd7990386e3fd235b
hhhhhq <Mr.j@tom.com>
2020-12-01 10:53:05 +0800

    完成基本 table

--- src/demo.vue
+++ src/demo.vue
@@ -11,15 +11,20 @@
           :order-by.sync="orderBy"
           @update:orderBy="x"
           :loading="loading"
-          height="400px"
+          :height="400"
+          expend-field="description"
+          checkable
       >
-
+        <template slot-scope="xxx">
+          <button @click="edit(xxx.item)">编辑</button>
+          <button @click="view(xxx.item)">查看</button>
+        </template>
       </g-table>
     </div>
 
-<!--    <div style="margin: 20px;">-->
-<!--      <g-table :columns="columns" :data-source="dataSource" bordered compact :striped="false"></g-table>-->
-<!--    </div>-->
+    <!--    <div style="margin: 20px;">-->
+    <!--      <g-table :columns="columns" :data-source="dataSource" bordered compact :striped="false"></g-table>-->
+    <!--    </div>-->
   </div>
 </template>
 
@@ -35,26 +40,26 @@ export default {
     return {
       selected: [],
       columns: [
-        {text: '姓名', field: 'name'},
-        {text: '分数', field: 'score'},
+        { text: '姓名', field: 'name', width: 100 },
+        { text: '分数', field: 'score', width: 200 },
       ],
       orderBy: {
         name: 'asc',
         score: 'desc'
       },
       dataSource: [
-        {id: 1, name: '张三', score: 100},
-        {id: 2, name: '李四', score: 99},
-        {id: 3, name: '王五', score: 98},
-        {id: 4, name: '周六', score: 97},
-        {id: 5, name: '吴七', score: 96},
-        {id: 6, name: '郑八', score: 95},
-        {id: 7, name: '张三', score: 94},
-        {id: 8, name: '李四', score: 93},
-        {id: 9, name: '王五', score: 92},
-        {id: 10, name: '周六', score: 91},
-        {id: 11, name: '吴七', score: 90},
-        {id: 12, name: '郑八', score: 89},
+        { id: 1, name: '张三', score: 100, description: 'xxx xxxx' },
+        { id: 2, name: '李四', score: 99, description: 'xxx xxxxxxx' },
+        { id: 3, name: '王五', score: 98 },
+        { id: 4, name: '周六', score: 97 },
+        { id: 5, name: '吴七', score: 96 },
+        { id: 6, name: '郑八', score: 95 },
+        { id: 7, name: '张三', score: 94 },
+        { id: 8, name: '李四', score: 93 },
+        { id: 9, name: '王五', score: 92 },
+        { id: 10, name: '周六', score: 91 },
+        { id: 11, name: '吴七', score: 90 },
+        { id: 12, name: '郑八', score: 89 },
       ],
       loading: false
     }
@@ -66,6 +71,12 @@ export default {
         this.dataSource = this.dataSource.sort((a, b) => a.score - b.score)
         this.loading = false
       }, 3000)
+    },
+    edit(item) {
+      alert(`开始编辑${item.id}`)
+    },
+    view(item) {
+      alert(`开始查看${item.id}`)
     }
   }
 };
--- src/table.vue
+++ src/table.vue
@@ -1,34 +1,51 @@
 <template>
   <div ref="wrapper" class="g-table-wrapper">
-    <div :style="{height, overflow: 'auto'}">
+    <div :style="{height, overflow: 'auto'}" ref="tableWrapper">
       <table ref="table" class="g-table" :class="{bordered, compact, striped: striped}">
         <thead>
-          <tr>
-            <th>
-              <input ref="allChecked" type="checkbox" @change="onChangeAllItems" :checked="areAllItemsSelected">
-            </th>
-            <th v-if="numberVisible">#</th>
-            <th v-for="column in columns" :key="column.field">
-              <div class="g-table-header">
-                {{ column.text }}
-                <span v-if="column.field in orderBy" class="g-table-sorter" @click="changeOrderBy(column.field)">
-                  <g-icon name="asc" :class="{active: orderBy[column['field']] === 'asc'}"></g-icon>
-                  <g-icon name="desc" :class="{active: orderBy[column['field']] === 'desc'}"></g-icon>
-                </span>
-              </div>
-            </th>
-          </tr>
+        <tr>
+          <th v-if="expendField" :style="{width: '50px'}" class="g-table-center"></th>
+          <th v-if="checkable" :style="{width: '50px'}" class="g-table-center">
+            <input ref="allChecked" type="checkbox" @change="onChangeAllItems" :checked="areAllItemsSelected">
+          </th>
+          <th :style="{width: '50px'}" v-if="numberVisible">#</th>
+          <th :style="{width: column.width + 'px'}" v-for="column in columns" :key="column.field">
+            <div class="g-table-header">
+              {{ column.text }}
+              <span v-if="column.field in orderBy" class="g-table-sorter" @click="changeOrderBy(column.field)">
+                <g-icon name="asc" :class="{active: orderBy[column['field']] === 'asc'}"></g-icon>
+                <g-icon name="desc" :class="{active: orderBy[column['field']] === 'desc'}"></g-icon>
+              </span>
+            </div>
+          </th>
+          <th ref="actionsHeader" v-if="$scopedSlots.default">操作</th>
+        </tr>
         </thead>
         <tbody>
-          <tr v-for="(item, index) in dataSource" :key="item.id">
-            <td>
+        <template v-for="(item, index) in dataSource">
+          <tr :key="item.id">
+            <td v-if="expendField" :style="{width: '50px'}" class="g-table-center">
+              <g-icon class="g-table-expendIcon" name="right" @click="expendItem(item.id)"></g-icon>
+            </td>
+            <td v-if="checkable" :style="{width: '50px'}" class="g-table-center">
               <input type="checkbox" @change="onChangeItem(item, index, $event)" :checked="inSelectedItems(item)">
             </td>
-            <td v-if="numberVisible">{{ index + 1 }}</td>
+            <td :style="{width: '50px'}" v-if="numberVisible">{{ index + 1 }}</td>
             <template v-for="column in columns">
-              <td :key="column.field">{{ item[column.field] }}</td>
+              <td :style="{width: column.width + 'px'}" :key="column.field">{{ item[column.field] }}</td>
             </template>
+            <td v-if="$scopedSlots.default">
+              <div ref="actions" style="display: inline-block">
+                <slot :item="item"></slot>
+              </div>
+            </td>
           </tr>
+          <tr v-if="inExpendedIds(item.id)" :key="`${item.id}-expend`">
+            <td :colspan="columns.length + expendedCellColSpan">
+              {{ item[expendField] || '空' }}
+            </td>
+          </tr>
+        </template>
         </tbody>
       </table>
     </div>
@@ -42,13 +59,21 @@
 import Icon from './icon'
 
 export default {
-  name: "GTable",
+  name: 'GTable',
   components: {
     'g-icon': Icon
   },
+  data() {
+    return {
+      expendedIds: []
+    }
+  },
   props: {
+    expendField: {
+      type: String
+    },
     height: {
-      type: [Number ,String]
+      type: Number
     },
     loading: {
       type: Boolean,
@@ -78,7 +103,7 @@ export default {
       type: Array,
       required: true,
       validator(array) {
-        return array.filter(item => item.id === undefined).length <= 0;
+        return array.filter(item => item.id === undefined).length <= 0
       }
     },
     numberVisible: {
@@ -86,6 +111,10 @@ export default {
       default: false
     },
     bordered: {
+      type: Boolean,
+      default: false
+    },
+    checkable: {
       type: Boolean,
       default: false
     }
@@ -106,13 +135,19 @@ export default {
       }
       return equal
       // this.dataSource.sort((a, b) => a.id - b.id)
+    },
+    expendedCellColSpan() {
+      let result = 0
+      if (this.checkable) {result += 1}
+      if (this.expendField) {result += 1}
+      return result
     }
   },
   watch: {
     selectedItems() {
-      if(this.selectedItems.length === this.dataSource.length) {
+      if (this.selectedItems.length === this.dataSource.length) {
         this.$refs.allChecked.indeterminate = false
-      }else if(this.selectedItems.length === 0) {
+      } else if (this.selectedItems.length === 0) {
         this.$refs.allChecked.indeterminate = false
       } else {
         this.$refs.allChecked.indeterminate = true
@@ -120,43 +155,71 @@ export default {
     }
   },
   mounted() {
-    let table2 = this.$refs.table.cloneNode(true)
+    let table2 = this.$refs.table.cloneNode(false)
     this.table2 = table2
     table2.classList.add('g-table-copy')
-    this.updateHeadersWidth()
+    let tHead = this.$refs.table.children[0]
+    let {height} = tHead.getBoundingClientRect()
+    this.$refs.tableWrapper.style.marginTop = height + 'px'
+    this.$refs.tableWrapper.style.height = this.height - height + 'px'
+    table2.appendChild(tHead)
     this.$refs.wrapper.appendChild(table2)
-    console.log(this.$refs.wrapper);
-    this.onWindowResize = () => this.updateHeadersWidth()
-    window.addEventListener('resize', this.onWindowResize)
+    // console.log(this.$refs.wrapper);
+
+    if(this.$scopedSlots.default) {
+      let div = this.$refs.actions[0]
+      let {width} = this.$refs.actions[0].getBoundingClientRect()
+      let parent = div.parentNode
+      let styles = getComputedStyle(parent)
+      let paddingLeft = styles.getPropertyValue('padding-left')
+      let paddingRight = styles.getPropertyValue('padding-right')
+      let borderLeft = styles.getPropertyValue('border-left-width')
+      let borderRight = styles.getPropertyValue('border-right-width')
+      console.log(paddingLeft, paddingRight, borderLeft, borderRight)
+      let width2 = width + parseInt(paddingLeft) + parseInt(paddingRight) + parseInt(borderLeft) + parseInt(borderRight) + 'px'
+      this.$refs.actionsHeader.style.width = width2
+      this.$refs.actions.map(div => {
+        div.parentNode.style.width = width2
+      })
+    }
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.onWindowResize)
     this.table2.remove()
   },
   methods: {
-    updateHeadersWidth() {
-      let tableHead = Array.from(this.$refs.table.children).filter(node => node.tagName.toLowerCase() === 'thead')[0]
-      let tableHead2
-      let table2 = this.table2
-      Array.from(table2.children).map(node => {
-        if(node.tagName.toLowerCase() !== 'thead') {
-          node.remove()
-        }else {
-          tableHead2 = node
-        }
-      })
-      // console.log(table2);
-      Array.from(tableHead.children[0].children).map((th, i) => {
-        const {width} = th.getBoundingClientRect()
-        tableHead2.children[0].children[i].style.width = width + 'px'
-      })
+    inExpendedIds(id) {
+      return this.expendedIds.indexOf(id) >= 0
     },
+    expendItem(id) {
+      if (this.inExpendedIds(id)) {
+        this.expendedIds.splice(this.expendedIds.indexOf(id), 1)
+      } else {
+        this.expendedIds.push(id)
+      }
+    },
+    // updateHeadersWidth() {
+    //   let tableHead = Array.from(this.$refs.table.children).filter(node => node.tagName.toLowerCase() === 'thead')[0]
+    //   let tableHead2
+    //   let table2 = this.table2
+    //   Array.from(table2.children).map(node => {
+    //     if(node.tagName.toLowerCase() !== 'thead') {
+    //       node.remove()
+    //     }else {
+    //       tableHead2 = node
+    //     }
+    //   })
+    //   // console.log(table2);
+    //   Array.from(tableHead.children[0].children).map((th, i) => {
+    //     const {width} = th.getBoundingClientRect()
+    //     tableHead2.children[0].children[i].style.width = width + 'px'
+    //   })
+    // },
     changeOrderBy(key) {
       const copy = JSON.parse(JSON.stringify(this.orderBy))
       let oldValue = copy[key]
-      if(oldValue ==='asc') {
+      if (oldValue === 'asc') {
         copy[key] = 'desc'
-      }else if(oldValue === 'desc') {
+      } else if (oldValue === 'desc') {
         copy[key] = true
       } else {
         copy[key] = 'asc'
@@ -166,7 +229,7 @@ export default {
     onChangeItem(item, index, e) {
       let selected = e.target.checked
       let copy = JSON.parse(JSON.stringify(this.selectedItems))
-      if(selected) {
+      if (selected) {
         copy.push(item)
       } else {
         copy = copy.filter(i => i.id !== item.id)
@@ -181,15 +244,17 @@ export default {
       return this.selectedItems.filter(i => i.id === item.id).length > 0
     }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
 @import "var";
+
 $grey: darken($grey, 10%);
 .g-table-wrapper {
   position: relative;
   overflow: auto;
+
   .g-table {
     width: 100%;
     border-collapse: collapse;
@@ -261,6 +326,10 @@ $grey: darken($grey, 10%);
       display: flex;
       align-items: center;
     }
+
+    .g-table-center {
+      text-align: center;
+    }
   }
 
   .g-table-loading {
@@ -280,6 +349,7 @@ $grey: darken($grey, 10%);
       @include spin;
     }
   }
+
   .g-table-copy {
     position: absolute;
     top: 0;
@@ -287,5 +357,11 @@ $grey: darken($grey, 10%);
     width: 100%;
     background-color: #fff;
   }
+
+  .g-table-expendIcon {
+    width: 10px;
+    height: 10px;
+  }
+
 }
 </style>
